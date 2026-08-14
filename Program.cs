6f3fd183959ea69b8dc06fbe3be3ae19bd27534e
astefanov <a.stefanov@sabris.de>
2026-08-14 15:45:10 +0200

    Auto-migrate on startup with connection resiliency

--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default"),
+        sql => sql.EnableRetryOnFailure()));
 builder.Services.AddScoped<PaymentPlanService>();
 builder.Services.AddScoped<ProfileService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 if (app.Environment.IsDevelopment())
 {
